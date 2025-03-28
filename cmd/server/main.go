a6c9d97027d69227069b43b4d1919f0266e0529a
Carlos Marques <carrlosmr@gmail.com>
2025-03-28 00:00:16 -0300

    refactor: adaptando para chamada das funções de rotas

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,7 +14,7 @@ import (
 	"github.com/marquescript/go-events/internal/infra/database"
 	"github.com/marquescript/go-events/internal/infra/factory"
 	"github.com/marquescript/go-events/internal/infra/http/middlewares"
-	"github.com/marquescript/go-events/internal/service"
+	"github.com/marquescript/go-events/internal/infra/http/routes"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
@@ -33,18 +32,7 @@ func main() {
 		log.Fatalf("erro ao carregar configurações: %v", err)
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		cfg.DBUser,
-		cfg.DBPassword,
-		cfg.DBHost,
-		cfg.DBPort,
-		cfg.DBName,
-	)
-
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatalf("erro ao abrir conexão com banco de dados: %v", err)
-	}
+	db := config.NewInstanceDatabase()
 	defer db.Close()
 
 	if err := db.Ping(); err != nil {
@@ -59,11 +47,6 @@ func main() {
 
 	log.Println("Migrations executadas com sucesso!")
 
-	userService := service.NewUserService(database.NewUser(db))
-
-	eventFactory := factory.NewEventFactory(db)
-	userFactory := factory.NewUserFactory(db)
-
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
@@ -77,22 +60,18 @@ func main() {
 		})
 	})
 
-	r.Route("/events", func(r chi.Router) {
-		r.Use(jwtauth.Verifier(cfg.TokenAuth))
-		r.Use(jwtauth.Authenticator)
+	userFactory := factory.NewUserFactory(db)
+	eventFactory := factory.NewEventFactory(db)
 
-		r.Use(middlewares.VerifyUserMiddleware(userService))
+	r.Group(func(c chi.Router) {
+		c.Use(jwtauth.Verifier(cfg.TokenAuth))
+		c.Use(jwtauth.Authenticator)
+		c.Use(middlewares.VerifyUserMiddleware(userFactory.Handler.UserService))
 
-		r.Post("/", eventFactory.Handler.CreateEvent)
-		r.Get("/{id}", eventFactory.Handler.FindEvent)
-		r.Get("/", eventFactory.Handler.FindAllEvents)
-		r.Put("/{eventId}", eventFactory.Handler.UpdateEvent)
-		r.Delete("/{eventId}", eventFactory.Handler.DeleteEvent)
-
+		routes.RegisterEventRoutes(c, eventFactory)
 	})
 
-	r.Post("/users", userFactory.Handler.CreateUser)
-	r.Post("/sign-in", userFactory.Handler.GetJWT)
+	routes.RegisterUserRoutes(r, userFactory)
 
 	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8080/docs/doc.json")))
 
